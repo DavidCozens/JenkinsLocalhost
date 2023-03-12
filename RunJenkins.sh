704ba98b765e8f4aafab3ceefdccf1bd3e51d409
David Cozens <David@cososo.co.uk>
2023-03-12 12:05:34 +0000

    fixed backup issues

--- RunJenkins.sh
+++ RunJenkins.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 docker-compose up &
+sleep 10
 while :
 do
     read -p "(B)ackup or (D)own:" prompt
     if [[ $prompt =~ [bB](ackup)* ]]
     then
-        docker run -w /var/jenkins_home --rm --volumes-from jenkinslocalhost-jenkins-1 -v $(pwd):/backup ubuntu tar czf "jenkinsbackup$(date +%Y%m%d%H%M%S).tgz" --exclude={"plugins","workspace",".cache","logs","war","secrets/master.key"} .
-        docker run -w /var/jenkins_home --rm --volumes-from jenkinslocalhost-jenkins-1 -v $(pwd):/backup ubuntu cp secrets/master.key "master.key"
+        echo "Backing Up"
+        docker run -w /var/jenkins_home --rm --volumes-from jenkinslocalhost-jenkins-1 -v $(pwd):/backup ubuntu tar czf "/backup/jenkinsbackup$(date +%Y%m%d%H%M%S).tgz" --exclude={"plugins","workspace",".cache","logs","war","secrets/master.key"} .
+        docker run -w /var/jenkins_home --rm --volumes-from jenkinslocalhost-jenkins-1 -v $(pwd):/backup ubuntu cp secrets/master.key "/backup/master.key"
         mv *.tgz "$backupdir"
     elif [[ $prompt =~ [dD](own)* ]]
     then
+        echo "Shuttting down"
         docker-compose down
         break
     fi
